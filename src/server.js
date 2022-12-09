e894c6295bd8d59119e5e9fab4024ee23a48bc45
Josana-Kla <josana.0205@gmail.com>
2022-12-09 20:32:58 -0300

    validate: post categories route

--- src/server.js
+++ src/server.js
@@ -4,11 +4,30 @@ import connection from './database/database.js';
 const app = express();
 app.use(express.json());
 
+async function checkCategoryExists(name) {
+    try {
+        const categoryExists = await connection.query(`
+            SELECT * FROM categories WHERE name=$1;
+        `, [name]);
+
+        if(categoryExists.rows[0].name === undefined) {
+            console.log("A categoria não existe! Crie-a!");
+            return false;
+        } else {
+            console.log("Essa categoria já existe!");
+            return true;
+        }
+    } catch (error) {
+        console.log(error);
+        console.log("Erro no servidor!");
+    }
+};
+
 // CATEGORIES ROUTES:
 app.get("/categories", async (req, res) => {
     try {
         const categories = await connection.query(`
-        SELECT * FROM categories;
+            SELECT * FROM categories;
         `);
 
         return res.status(200).send(categories.rows);
@@ -20,10 +39,18 @@ app.get("/categories", async (req, res) => {
 
 app.post("/categories", async (req, res) => {
     const { name } = req.body;
+
+    if(!name) {
+        return res.sendStatus(400);
+    };
     
+    if(await checkCategoryExists(name)) {
+        return res.sendStatus(409);
+    };
+
     try {
         await connection.query(`
-        INSERT INTO categories(name) VALUES($1)
+            INSERT INTO categories(name) VALUES($1)
         `, [name]);
 
         return res.sendStatus(201);
