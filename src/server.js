70b9b57f970d062853904ab088334382f7854376
Josana-Kla <josana.0205@gmail.com>
2022-12-09 19:19:09 -0300

    feat: added get categories route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import connection from './database/database.js';
 
 const app = express();
+
+app.get("/categories", async (req, res) => {
+    try {
+        const categories = await connection.query(`
+        SELECT * FROM categories;
+        `);
+
+        return res.status(200).send(categories.rows);
+    } catch(error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+});
 
 app.listen(4000);
