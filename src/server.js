1492c1fdd48e0da9c563501176d655156d8df8a8
Josana-Kla <josana.0205@gmail.com>
2022-12-09 19:37:35 -0300

    feat: added post categories route

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import connection from './database/database.js';
 
 const app = express();
+app.use(express.json());
 
+// CATEGORIES ROUTES:
 app.get("/categories", async (req, res) => {
     try {
         const categories = await connection.query(`
@@ -10,7 +12,22 @@ app.get("/categories", async (req, res) => {
         `);
 
         return res.status(200).send(categories.rows);
-    } catch(error) {
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+});
+
+app.post("/categories", async (req, res) => {
+    const { name } = req.body;
+    
+    try {
+        await connection.query(`
+        INSERT INTO categories(name) VALUES($1)
+        `, [name]);
+
+        return res.sendStatus(201);
+    } catch (error) {
         console.log(error);
         return res.sendStatus(500);
     }
