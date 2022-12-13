ffa1c282ec4eb428ddccfa6d37feb4ba02c19cc7
Josana-Kla <josana.0205@gmail.com>
2022-12-12 23:41:06 -0300

    file: organized rentals routes in controller and route files

--- src/routers/rentals.routers.js
+++ src/routers/rentals.routers.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import { getRentals, createRentals, updateRentals, deleteRentals } from '../controllers/rentals.controller.js';
 
 const router = express.Router();
 
-router.post("", );
-router.post("", );
-router.get("", );
+router.get("/rentals", getRentals);
+router.post("/rentals", createRentals);
+router.put("/rentals/:id/return", updateRentals);
+router.delete("/rentals/:id", deleteRentals);
 
 export default router;
