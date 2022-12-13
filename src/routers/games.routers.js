663b3c82dbf1857d5053f812c575443623f862c0
Josana-Kla <josana.0205@gmail.com>
2022-12-12 22:35:52 -0300

    file: organized games routes in controller and route files

--- src/routers/games.routers.js
+++ src/routers/games.routers.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { getGames, createGames } from '../controllers/games.controllers.js';
 
 const router = express.Router();
 
+router.get("/games", getGames);
+router.post("/games", createGames);
 router.post("", );
-router.post("", );
-router.get("", );
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import joi from 'joi';
 import DateExtension from '@joi/date';
 import dayjs from 'dayjs';
 import categoryRouter from './routers/categories.routers.js';
+import gameRouter from './routers/games.routers.js';
 
 const app = express();
 app.use(cors());
@@ -14,14 +15,8 @@ const joii = joi.extend(DateExtension);
 
 // Routes:
 app.use(categoryRouter);
+app.use(gameRouter);
 
-const gamesSchema = joi.object({
-    name: joi.string().pattern(/^[A-zÀ-ú]/).required().empty(' '),
-    image: joi.string().uri().required().empty(' '),
-    stockTotal: joi.number().integer().greater(0).required(),
-    categoryId: joi.number().integer().required(),
-    pricePerDay: joi.number().integer().greater(0).required()
-}); 
 
 const customersSchema = joi.object({
     name: joi.string().pattern(/^[A-zÀ-ú]/).required().empty(' '),
@@ -39,44 +34,6 @@ const rentalsSchema = joi.object({
 //FUNCTIONS:
 function currentDate() {
     return dayjs(new Date()).format('YYYY-MM-DD');
-};
-
-async function checkGameExists(name) {
-    try {
-        const gameExists = await connection.query(`
-            SELECT * FROM games WHERE name=$1;
-        `, [name]);
-
-        if(gameExists.rows[0].name === undefined) {
-            console.log("O jogo não existe! Crie-o!");
-            return false;
-        } else {
-            console.log("Esse jogo já existe!");
-            return true;
-        }
-    } catch (error) {
-        console.log(error);
-        console.log("Erro no servidor ao verificar se o jogo existe!");
-    }
-};
-
-async function checkIdCategoryExists(id) {
-    try {
-        const sameId = await connection.query(`
-            SELECT * FROM categories WHERE id = $1;
-        `, [id]);
-
-        if(sameId.rows[0].id === undefined) {
-            console.log("O id não existe! Escolha um id existente ou crie uma nova categoria!");
-            return true;
-        } else {
-            console.log("Esse id de categoria existe! 😃");
-            return false;
-        }
-    } catch (error) {
-        console.log(error);
-        console.log("Erro no servidor ao verificar se o id da categoria existe!");
-    }
 };
 
 async function checkCpfExists(cpf) {
@@ -247,61 +204,6 @@ async function checkMovieNotReturned(id) {
     }
 };
 
-// GAMES ROUTES:
-app.get("/games", async (req, res) => {
-    const { name } = req.query;
-
-    try {
-        let games;
-
-        if(name) {
-            games = await connection.query(`
-                SELECT * FROM games WHERE name ILIKE($1 || '%');
-            `, [name]);
-
-            return res.status(200).send(games.rows);
-        } else {
-            games = await connection.query(`
-                SELECT * FROM games;
-            `);
-
-            return res.status(200).send(games.rows);
-        }
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(500);
-    }
-});
-
-app.post("/games", async (req, res) => {
-    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
-    const validation = gamesSchema.validate(req.body, {abortEarly: false});
-
-    if(validation.error) {
-        const error = validation.error.details.map(detail => detail.message);
-
-        return res.status(400).send(error);
-    };
-
-    if(await checkIdCategoryExists(categoryId)) {
-        return res.sendStatus(400);
-    };
-
-    if(await checkGameExists(name)) {
-        return res.sendStatus(409);
-    };
-
-    try {
-        await connection.query(`
-            INSERT INTO games(name, image, "stockTotal", "categoryId", "pricePerDay") VALUES($1, $2, $3, $4, $5)
-        `, [name, image, stockTotal, categoryId, pricePerDay]);
-
-        return res.sendStatus(201);
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(500);
-    }
-});
 
 // CLIENTES ROUTES:
 app.get("/customers", async (req, res) => {
